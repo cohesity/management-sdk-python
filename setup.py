5448a2212fcf53f59d8a53ce16fadb5ced3afd56
ashish-cohesity <46462071+ashish-cohesity@users.noreply.github.com>
2019-04-16 14:40:26 -0700

    Update setup.py for version 1.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except(IOError, ImportError):
 
 setup(
     name='cohesity_management_sdk',
-    version='1.0.0',
+    version='1.0.1',
     description='This SDK provides operations for interfacing with the Cohesity Cluster.',
     long_description=long_description,
     author='Cohesity Inc.',
