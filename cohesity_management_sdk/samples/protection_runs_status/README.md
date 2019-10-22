64e172f24a763cfa1a72e433b542240e8e831978
Anoop B <anoop.bhat@gmail.com>
2019-10-21 22:29:12 -0500

    added sample for listing protection runs status

---
+++ cohesity_management_sdk/samples/protection_runs_status/README.md
@@ -0,0 +1,20 @@
+Samples for Cohesity Management SDK 
+========================
+This repository contains reference samples for Cohesity Management SDK.
+ 
+### Get Protection Run Status
+
+[README](protection_run_status/README.md)
+
+### Handling Errors.
+[README](handling_errors/README.md)
+
+### List Protection Jobs.
+[README](list_protection_jobs/README.md)
+
+### List Unresolved Alerts.
+[README](list_unresolved_alerts/README.md)
+
+### Running a Protection Job on demand.
+[README](on_demand_job_run/README.md)
+
---
+++ cohesity_management_sdk/samples/protection_runs_status/protection_run_status.py
@@ -0,0 +1,53 @@
+# Copyright 2019 Cohesity Inc.
+#
+# Python example to get a status of a protection run
+#
+# Usage: python protection_run_status.py
+
+import datetime
+
+from cohesity_management_sdk.cohesity_client import CohesityClient
+
+CLUSTER_USERNAME = 'USERNAME'
+CLUSTER_PASSWORD = 'PASSWORD'
+CLUSTER_VIP = 'VIP_OR_FQDN'
+DOMAIN = 'LOCAL'
+
+#job_id = 
+
+class ProtectionRunsList(object):
+
+    def display_protection_runs(self, cohesity_client):
+        """
+        Method to display the list of protection runs
+        :param cohesity_client(object): Cohesity client object.
+        :return:
+        """
+        protection_runs = cohesity_client.protection_runs
+        # run_list = protection_runs.get_protection_runs(job_id)
+        run_list = protection_runs.get_protection_runs()
+        for run in run_list:
+            print(self.epoch_to_date(run.backup_run.stats.start_time_usecs), run.backup_run.job_run_id, run.backup_run.status)
+
+    @staticmethod
+    def epoch_to_date(epoch):
+        """
+        Method to convert epoch time in usec to date format
+        :param epoch(int): Epoch time of the job run.
+        :return: date(str): Date format of the job runj.
+        """
+        date = datetime.datetime.fromtimestamp(epoch/10**6).\
+            strftime('%m-%d-%Y %H:%M:%S')
+        return date
+
+def main():
+
+    cohesity_client = CohesityClient(cluster_vip=CLUSTER_VIP,
+                                     username=CLUSTER_USERNAME,
+                                     password=CLUSTER_PASSWORD,
+				                     domain=DOMAIN)
+    protection_runs = ProtectionRunsList()
+    protection_runs.display_protection_runs(cohesity_client)
+
+if __name__ == '__main__':
+    main()
