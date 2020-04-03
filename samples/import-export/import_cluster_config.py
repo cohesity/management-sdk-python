17c7cc1c17b54604b666470753cf52fa0716d506
naveena.maplelabs <naveena.maplelabs@cohesity.com>
2020-04-03 21:43:11 +0530

    Initial commit for import tasks

--- samples/import-export/import_cluster_config.py
+++ samples/import-export/import_cluster_config.py
@@ -2,6 +2,9 @@ import json
 import pickle
 import configparser
 from cohesity_management_sdk.cohesity_client import CohesityClient
+from cohesity_management_sdk.models.protection_policy_request import ProtectionPolicyRequest
+from cohesity_management_sdk.models.create_view_request import CreateViewRequest
+from cohesity_management_sdk.models.create_view_box_params import CreateViewBoxParams
 from cohesity_management_sdk.exceptions.request_error_error_exception import RequestErrorErrorException
 from cohesity_management_sdk.exceptions.api_exception import APIException
 
@@ -11,15 +14,16 @@ configparser = configparser.ConfigParser()
 configparser.read('config.ini')
 
 cohesity_client = CohesityClient(cluster_vip=configparser.get('import_cluster_config', 'cluster_ip'),
-                                 username=configparser.get('import_cluster_config', 'username'),
-                                 password=configparser.get('import_cluster_config', 'password'),
-                                 domain= configparser.get('import_cluster_config', 'domain'))
+                                 username=configparser.get(
+                                     'import_cluster_config', 'username'),
+                                 password=configparser.get(
+                                     'import_cluster_config', 'password'),
+                                 domain=configparser.get('import_cluster_config', 'domain'))
 
 cluster_dict = pickle.load(open("cluster_config.txt", "rb"))
 
 with open("template.json", "r") as file_obj:
     template_dict = json.load(file_obj)
-
 
 
 def create_protection_policy():
@@ -29,40 +33,45 @@ def create_protection_policy():
     """
     protection_policy_list = cluster_dict.get("policies", [])
     try:
-        if not protection_policy_list:
-            print("No existing protection policies available.")
-            protection_policy = template_dict["policy"]
+        if not protection_policy_list or protection_policy_list[0].mtype == "kRPO":
+            protection_policy = ProtectionPolicyRequest()
+            scheduling_policy = {"periodicity": "kMonthly",
+                                 "monthlySchedule": {
+                                     "dayCount": "kFirst",
+                                     "day": "kSunday"
+                                 }}
+            protection_policy.incremental_scheduling_policy = scheduling_policy
         else:
             protection_policy = protection_policy_list[0]
-            protection_policy.name = "TestPolicy"
+        protection_policy.name = "TestPolicy"
 
         result = cohesity_client.protection_policies.create_protection_policy(
             protection_policy)
-    except RequestErrorErrorException as e: 
+        print(result)
+    except RequestErrorErrorException as e:
         print(e)
-    except APIException as e: 
+    except APIException as e:
         print(e)
     except Exception as err:
         print(err)
+
 
 def create_storage_domain():
     try:
         storage_domain_list = cluster_dict.get("storage_domains", [])
         if not storage_domain_list:
-            storage_domain = template_dict["storage_domain"]
+            storage_domain = CreateViewBoxParams()
             cluster_partition_id = cohesity_client.cluster_partitions.\
                 get_cluster_partitions()[0].id
-            storage_domain["name"] = "TestSD"
-            storage_domain["id"] = cluster_partition_id
+            storage_domain.cluster_partition_id = cluster_partition_id
         else:
-            # If the view box if available, modified name.
             storage_domain = storage_domain_list[0]
-            storage_domain.name = "TestSD"
+        storage_domain.name = "TestSD"
         result = cohesity_client.view_boxes.create_view_box(storage_domain)
         print(result)
-    except RequestErrorErrorException as e: 
+    except RequestErrorErrorException as e:
         print(e)
-    except APIException as e: 
+    except APIException as e:
         print(e)
     except Exception as err:
         print(err)
@@ -72,22 +81,22 @@ def create_view():
     view_list = cluster_dict.get("views", [])
     try:
         if not view_list:
-            view = template_dict["view"]
+            view = CreateViewRequest()
             view_box_id = cohesity_client.view_boxes.get_view_boxes()[0].id
-            view["viewBoxId"] = view_box_id
+            view.view_box_id = view_box_id
         else:
             view = view_list[0]
-            view.name = "TestView"
-        cohesity_client.views.create_view(view)
-    except RequestErrorErrorException as e: 
+        view.name = "TestView"
+        result = cohesity_client.views.create_view(view)
+        print(result)
+    except RequestErrorErrorException as e:
         print(e)
-    except APIException as e: 
+    except APIException as e:
         print(e)
     except Exception as err:
         print(err)
 
 
-
 # create_protection_policy()
 # create_storage_domain()
-create_view()
+# create_view()
