689939f2364fd9f8eaf6ef57bca73c02dfedffa7
naveena-maplelabs <naveena.maplelabs@cohesity.com>
2021-02-22 13:53:09 +0530

    Patch release for 6.5.1- Datalock config Added

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except(IOError, ImportError):
 
 setup(
     name='cohesity_management_sdk',
-    version='1.5.2',
+    version='1.5.3',
     description='This SDK provides operations for interfacing with the Cohesity Cluster.',
     long_description=long_description,
     long_description_content_type=long_description_content_type,
